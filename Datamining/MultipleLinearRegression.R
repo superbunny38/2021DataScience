002e035c649f4a4b28e6b96d53574003f55da9ae
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2022-04-09 00:16:09 +0900

    Add files via upload

--- Datamining/MultipleLinearRegression.R
+++ Datamining/MultipleLinearRegression.R
@@ -1,55 +1,58 @@
-#R-Exercise: MLR (1)
+### Linar regression model of price vs. car attributes
+car.df <- read.csv("ToyotaCorolla.csv")
 
-car.df <- read.csv("ToyotaCorolla.csv")#csv 파일 불러오기
-car.df <- car.df[1:1000,]#1000 rows 선택
-head(car.df)#첫 여섯 행 출력
-dim(car.df)#data size
-summary(car.df)#요약
-str(car.df)#구조
-names(car.df)#feature names
+#use first 1000 rows of data
+car.df <- car.df[1:1000,]
+dim(car.df)
+head(car.df)
 
-selected.var <- c(3,4,7:10,12:14,17,18)#변수 index 선택
-length(selected.var)#11개 변수 선택
-selected.var#선택된 변수 번호
+#select variables for regression
+selected.var <- c(3,4,7,8,9,10,12,13,14,17,18)
 
-set.seed(1)#항상 동일한 값이 나오도록 seed값 고정
+#partition data
+set.seed(1)#set seed for reproducing the partition
 
-train.index <- sample(c(1:1000),600)#training data rows 선택
-train.df <- car.df[train.index, selected.var]#training dataset
-valid.df <- car.df[-train.index, selected.var]#validation datset (training dataset에 할당한 row는 제외)
+train.index <- sample(c(1:1000),600)
+train.df <- car.df[train.index,selected.var]
+valid.df <- car.df[-train.index, selected.var]
 
-car.lm <- lm(Price ~., data = train.df)#linear model 형태로 training dataset으로 학습 해 Price를 예측하는 모델 생성
-options(scipen = 999)#scipen: 지수표기를 숫자표기로 바꿈
-summary(car.lm)#모델 요약
+#Use lm() to run a linear regression of Price on all 11 predictors in the training set.
+#use . after ~ to include all the remaining columns in train.df as predictors.
+
+car.lm <- lm(Price ~., data=train.df)
+
+#Use options() to ensure the numbers are not displayed in scientific notation.
+options(scipen = 999)
+summary(car.lm)
+
+### Predicted Prices (and Errors) for 20 cars in validation set and summary predictive measures for entire validation set
+
+library(forecast)
+
+car.lm.predict <- predict(car.lm, valid.df)#make predictions on a new set
+options(scipen = 999, digits = 0)
+
+some.residuals <- valid.df$Price[1:20] - car.lm.predict[1:20]#y-y_hat
+data.frame("Predicted" = car.lm.predict[1:20], "Actual" = valid.df$Price[1:20], "Residual" = some.residuals)
+
+accuracy(car.lm.predict, valid.df$Price)#compute common accuracy measures
+
+#code for plotting histogram of validation errors
+library(forecast)
+car.lm.predict <- predict(car.lm, valid.df)
+all.residuals <- valid.df$Price - car.lm.predict
+class(all.residuals)
+hist(all.residuals, breaks = 25, xlab="Residuals", main="")
+length(all.residuals[which(all.residuals<-1460 & all.residuals <1460)])/400#여기에 넣어야 실행됨,, 이 코드가 뭐하는 거지?
+
+hist(valid.df$Price)
 
 
-#R-Exercise: MLR (2)
 
-car.lm.pred <- predict(car.lm, valid.df)#car.lm 모델을 이용해서 validation data를 예측
+## Exhaustive search for reducing predictors in toyota corolla example
 
-options(scipen = 999, digits = 0)#지수표기를 숫자표기로 바꾸고 소수점 출력 안 함
-some.residuals <- valid.df$Price[1:20]-car.lm.pred[1:20]#잔차 계산 (y-y_hat)
-data.frame("Predicted" = car.lm.pred[1:20],#예측한 값
-           "Actual" = valid.df$Price[1:20],#실제 값
-           "Residual" =some.residuals)#잔차
+#unlike with lm, categorical predictors must be turned into dummies manually.
 
-library(forecast)#forecast 패키지 불러오기
-options(scipen = 999, digits = 3)#지수표기를 숫자표기로 바꿈 + 소수점 두 번째 자리까지 표시
-accuracy(car.lm.pred, valid.df$Price)#정확성 관련 오류
-
-
-#R-Exercise: MLR (3)
-all.residuals <- valid.df$Price - car.lm.pred
-length(all.residuals[which(all.residuals >- 1406&all.residuals<1406)])/400
-
-#잔차를 25개 범위로 나눠 히스토그램을 통한 시각화
-hist(all.residuals, breaks = 25, xlab = "Residuals", main = "")
-
-boxplot(all.residuals)#잔차를 박스 플롯으로 시각화
-
-
-#R-Exercise: Selecting Subsets (1)
-#install.packages("leaps")
 
 library(leaps)#leaps 패키지 불러오기
 class(car.df$Fuel_Type)
@@ -63,28 +66,37 @@ head(train.df)
 
 search <- regsubsets(Price ~., data = train.df, nbest = 1, nvmax = dim(train.df)[2], method = "exhaustive")
 sum <- summary(search)
+
+#show models
 sum$which
 
-
-#R-Exercise: Selecting Subsets (2)
-
-sum$rsq
-
+#show metrics
+sum$rsq#결과가 이상한데..?
+#R squared values range from 0 to 1.. but.. all 1s
 sum$adjr2
-
 sum$cp
 
 
-#R-Exercise: Selecting Subsets (3)
+## Backward Elimination for reducing predictors in toyota corolla example
 
-car.lm <- lm(Price ~ ., data = train.df)#train linear model
-car.lm.step <- step(car.lm, direction = "forward")#forward selection
-summary(car.lm.step)#전진 선택 결과 요약(모든 변수 다 쓰임)
+#use step() to run stepwise regression
+#set directions = to either "backward", "forward", or "both".
+
+car.lm <- lm(Price ~., data = train.df)
+car.lm.step <- step(car.lm, direction = "backward")
+summary(car.lm.step)#which variables did it drop
+head(car.lm.step)
+
+Fuel_Type_val <- as.data.frame(model.matrix(~0 + Fuel_Type, data = valid.df))
+valid.df <- cbind(valid.df[,-4], Fuel_Type_val[,c(1,2)])
+car.lm.step.pred <- predict(car.lm.step, valid.df)
+accuracy(car.lm.step.pred, valid.df$Price)
 
 
-#R-Exercise: Selecting Subsets (4)
+## Forward Elimination for reducingpredictors in toyota corolla example
+car.lm.forward <- step(car.lm, direction = "forward")
+summary(car.lm.forward)
 
-car.lm <- lm(Price ~., data = train.df)#train a linear model
-car.lm.step <- step(car.lm, direction = "backward")#backward selection
-summary(car.lm.step)#후진 선택 요약
-
+#Stepwise Regression
+car.lm.stepwise <- step(car.lm, direction = "both")
+summary(car.lm.stepwise)
